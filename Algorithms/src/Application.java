ea25dd50fbbb6d900eee2c516f8a4dc7f0883b2e
Marius-Daniel <marius.daniel.calarasu@gmail.com>
2020-05-21 23:22:54 +0300

    i added BinarySearch algorithm

--- Algorithms/src/Application.java
+++ Algorithms/src/Application.java
@@ -22,7 +22,12 @@ public class Application {
         long start = System.currentTimeMillis();
         System.out.println(FibonacciSeries.fibonacci(100_000));
         long end = System.currentTimeMillis();
-        System.out.println((end -start));
+        System.out.println((end - start));
+
+        System.out.println("---- Print index of a given element of an array using BinarySearch ----");
+        BinarySearch binarySearch = new BinarySearch();
+        int[] num = new int[]{1, 5, 8, 12, 24, 25, 33, 34, 54, 62};
+        System.out.println(binarySearch.searchElement(num, 25));
 
     }
 }
