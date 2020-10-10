f9e46c85abd333b91e58d19cc1b6e5fce2a1f91c
Marius-Daniel <marius.daniel.calarasu@gmail.com>
2020-10-10 09:59:14 +0300

    refacut

--- RecapitulareEx2/src/Main.java
+++ RecapitulareEx2/src/Main.java
@@ -2,11 +2,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int []  numbers = {1,2,3,3,4,5,5,8,9,10};
+        int [] numbers2 = {1,3,2,2,4,5,8,5,10};
+
+        int []  numbers = {1,2,3,3,4,5,5,8,9,10,10};
         int countDuplicate = 0;
-        for (int i = 0 ; i <numbers.length-1; i++){
-            if(numbers[i] == numbers[i +1]){
-                countDuplicate++;
+
+
+        for (int i = 0 ; i <numbers2.length-1; i++){
+            for(int j = 0 ; j < i; j++){
+                if(numbers2[i] == numbers2[j]){
+                    countDuplicate++;
+                }
             }
         }
         System.out.println(countDuplicate);
